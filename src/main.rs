d128e393885d5185fd00cffc996c1fe55beed6f5
LeoFabrial <leofabrial@gmail.com>
2022-09-21 21:19:50 +0800

    initial solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    print!("Initial Commit");
+    let width = 7;
+    make_tree(width);
 }
+
+fn make_tree(width: u8) {
+    let format_width: String = width.to_string();
+    for i in 1..width {
+        if i % 2 != 0 {
+            [3,333,33333]
+            let vec: Vec<i32> = vec![3; i.into()];
+            let format = concat!["{:^", format_width, '}'];
+            println!("format", ); //
+        }
+    }
+}
+
+// fn test(width: u8) {
+//     for i in 1..width {
+//         let x = i;
+//         println!("{}", x);
+//     }
+// }
