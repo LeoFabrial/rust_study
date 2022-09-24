8cbf4dc85667c9696413065c1bf2becda6babe5e
LeoFabrial <leofabrial@gmail.com>
2022-09-24 15:07:03 +0800

    solved

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
 fn main() {
-    let width = 7;
-    make_tree(width);
-}
+    let base: usize = 9;
 
-fn make_tree(width: u8) {
-    let format_width: String = width.to_string();
-    for i in 1..width {
+    for i in 1..base + 1 {
         if i % 2 != 0 {
-            [3,333,33333]
-            let vec: Vec<i32> = vec![3; i.into()];
-            let format = concat!["{:^", format_width, '}'];
-            println!("format", ); //
+            println!("{:^1$}", "3".repeat(i), base);
         }
     }
 }
-
-// fn test(width: u8) {
-//     for i in 1..width {
-//         let x = i;
-//         println!("{}", x);
-//     }
-// }
