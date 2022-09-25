5ec66f985c9f4101e3d1019f97fb0fc6fdfd96e3
LeoFabrial <leofabrial@gmail.com>
2022-09-25 15:37:42 +0800

    final xmas three solution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let base: usize = 9;
+    let base: usize = 41;
 
     for i in 1..base + 1 {
         if i % 2 != 0 {
             println!("{:^1$}", "3".repeat(i), base);
         }
     }
+
+    println!("{:^1$}", "|||", base);
 }
