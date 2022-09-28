2e77dfc2236a73c7a9ccd87943db818dc2d0025f
LeoFabrial <leofabrial@gmail.com>
2022-09-28 20:18:11 +0800

    finished challenge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    print!("Initial Commit");
+    fibonacci(7);
+}
+
+fn fibonacci(n: u8) -> u128 {
+    let mut result: u128 = 0;
+    let mut prev: u128 = 0;
+    let mut curr: u128 = 1;
+
+    for i in 1..=n {
+        result = prev + curr; // 1, 2
+        if dbg!(i) > 1 {
+            prev = curr; // 0 -> 1
+            curr = result; // 1 -> 1
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn output_test() {
+        assert_eq!(fibonacci(3), 2);
+        assert_eq!(fibonacci(7), 13);
+        assert_eq!(fibonacci(8), 21);
+        assert_eq!(fibonacci(124), 36726740705505779255899443);
+        assert_eq!(fibonacci(125), 59425114757512643212875125);
+        assert_eq!(fibonacci(185), 205697230343233228174223751303346572685);
+    }
 }
