f3718f61be4069874904213817a3348094674475
LeoFabrial <leofabrial@gmail.com>
2022-10-02 17:11:14 +0800

    serde study

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    print!("Initial Commit");
+// use serde_json::Result;
+use serde_json::Value;
+use std::{error::Error, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let foo = fs::read_to_string("data/proxy.json")?;
+    println!("My JSON {}", foo);
+    let data: Value = serde_json::from_str(&foo)?;
+    println!("{}", data["id"]);
+
+    Ok(())
 }
